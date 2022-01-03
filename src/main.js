12aa159706318bb5fb26d782104aecd3660fc73e
CietrzewToJa <64318229+CietrzewToJa@users.noreply.github.com>
2022-01-03 12:40:21 +0100

    Add error handling and input validation

--- src/main.js
+++ src/main.js
@@ -31,7 +31,10 @@ class WeatherApp {
             getWeatherByCity(query)
                 .then(data => {
                     this.displayWeatherData(data)
-                });
+                }).catch(() => {
+                    this.fadeInOut();
+                    this.viewElems.searchInput.style.borderColor = 'red';
+            });
         }
     }
 
@@ -80,106 +83,5 @@ class WeatherApp {
         this.viewElems.weatherMaxTemp.innerText = `Max temperature: ${maxTemp} °C`
     }
 }
-
-// const viewElems = {};
-
-// const getDOMElem = id => {
-//     return document.getElementById(id);
-// }
-
-// const connectHTMLElems = () => {
-//     viewElems.mainContainer = getDOMElem('mainContainer');
-//     viewElems.weatherSearchView = getDOMElem('weatherSearchView');
-//     viewElems.weatherForecastView = getDOMElem('weatherForecastView');
-//
-//     viewElems.searchInput = getDOMElem('searchInput');
-//     viewElems.searchButton = getDOMElem('searchButton');
-//     viewElems.weatherCityContainer = getDOMElem('weatherCityContainer');
-//
-//     viewElems.weatherCity = getDOMElem('weatherCity');
-//     viewElems.weatherIcon = getDOMElem('weatherIcon');
-//
-//     viewElems.weatherCurrentTemp = getDOMElem('weatherCurrentTemp');
-//     viewElems.weatherMinTemp = getDOMElem('weatherMinTemp');
-//     viewElems.weatherMaxTemp = getDOMElem('weatherMaxTemp');
-//
-//     viewElems.returnToSearchButton = getDOMElem('returnToSearchBtn');
-// }
-
-// const setupListeners = () => {
-//     viewElems.searchInput.addEventListener('keydown', onEnterSubmit);
-//     viewElems.searchButton.addEventListener('click', onClickSubmit);
-//     viewElems.returnToSearchButton.addEventListener('click', returnToSearch);
-// }
-
-// const initializeApp = () => {
-//     connectHTMLElems();
-//     setupListeners();
-// }
-
-// const onClickSubmit = () => {
-//         fadeInOut()
-//         let query = viewElems.searchInput.value;
-//         getWeatherByCity(query)
-//             .then(data => {
-//                 displayWeatherData(data)
-//             });
-// }
-
-// const onEnterSubmit = event => {
-//     if(event.key === 'Enter') {
-//         fadeInOut();
-//         let query = viewElems.searchInput.value;
-//         getWeatherByCity(query)
-//             .then(data => {
-//                 displayWeatherData(data)
-//             });
-//     }
-// }
-
-// const displayWeatherData = data => {
-//     switchView()
-//     fadeInOut()
-//
-//     const weather = data.consolidated_weather[0];
-//
-//     viewElems.weatherCity.innerText = data.title;
-//     viewElems.weatherIcon.src = `https://www.metaweather.com/static/img/weather/${weather.weather_state_abbr}.svg`;
-//     viewElems.weatherIcon.alt = weather.weather_state_alt;
-//
-//     const currentTemp = weather.the_temp.toFixed(2);
-//     const maxTemp = weather.min_temp.toFixed(2);
-//     const minTemp = weather.max_temp.toFixed(2);
-//
-//     viewElems.weatherCurrentTemp.innerText = `Current temperature: ${currentTemp} °C`
-//     viewElems.weatherMinTemp.innerText = `Min temperature: ${minTemp} °C`
-//     viewElems.weatherMaxTemp.innerText = `Max temperature: ${maxTemp} °C`
-// }
-
-// const fadeInOut = () => {
-//     if(viewElems.mainContainer.style.opacity === '1' || viewElems.mainContainer.style.opacity === '') {
-//         viewElems.mainContainer.style.opacity = '0';
-//     } else {
-//         viewElems.mainContainer.style.opacity = '1';
-//     }
-// }
-
-// const switchView = () => {
-//     if(viewElems.weatherSearchView.style.display !== 'none') {
-//         viewElems.weatherSearchView.style.display = 'none'
-//         viewElems.weatherForecastView.style.display = 'block'
-//     } else {
-//         viewElems.weatherForecastView.style.display = 'none'
-//         viewElems.weatherSearchView.style.display = 'flex'
-//     }
-// }
-
-// const returnToSearch = () => {
-//     fadeInOut()
-//     setTimeout(() => {
-//         switchView();
-//         fadeInOut()
-//     }, 500)
-// }
 
 document.addEventListener('DOMContentLoaded', new WeatherApp())
